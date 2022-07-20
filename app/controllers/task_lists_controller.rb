bdbaf77135ade20d8a1718e10d1822caaa21bee4
dyrkon <dyrkon603@gmail.com>
2022-07-20 09:36:18 +0200

    Seeds added. Views refactored

--- app/controllers/task_lists_controller.rb
+++ app/controllers/task_lists_controller.rb
@@ -7,6 +7,7 @@ class TaskListsController < ApplicationController
 
   def show
     @task_list = TaskList.find(params[:id])
+    @tasks = Task.where(:task_list_id => @task_list.id)
   end
 
   def new; end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,9 @@
 # frozen_string_literal: true
 
+TaskList.create
+TaskList.create
+
+Task.create(:name => "Clean up 1", :description => "Clean up the half of the rooms", :deadline => Date.today-50, :task_list_id => 1)
+Task.create(:name => "Clean up 2", :description => "Clean up the other half of the rooms", :deadline => Date.today+40, :task_list_id => 1)
+Task.create(:name => "Do the dishes", :description => "Do the dishes", :deadline => Date.today-10, :task_list_id => 2)
+Task.create(:name => "Put the dishes back", :description => "Put the dishes back where they belong", :deadline => Date.today+30, :task_list_id => 2)
