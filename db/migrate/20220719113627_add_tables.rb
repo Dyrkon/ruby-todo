8db9b2827485961d0281fe0c9079724d2b58162a
dyrkon <dyrkon603@gmail.com>
2022-07-20 09:43:13 +0200

    Second batch of requested changes implemented

--- db/migrate/20220719113627_add_tables.rb
+++ db/migrate/20220719113627_add_tables.rb
@@ -2,14 +2,14 @@ class AddTables < ActiveRecord::Migration[7.0]
   def change
     create_table :task_lists do |t|
       t.boolean :hidden, default: false
-
+      t.string :name, null: false
       t.timestamps
     end
 
     create_table :tasks do |t|
       t.string :name, null: false
       t.string :description
-      t.timestamp :deadline, null: false
+      t.timestamp :deadline
       t.integer :task_list_id, null: false
       t.boolean :done, default: false
 
